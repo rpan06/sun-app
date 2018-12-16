420684d3e16fc5d8a157aed0e98107520ef25384
krow <rpan06@gmail.com>
2018-12-15 16:45:36 -0800

    added zipcode js

--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -1,14 +1,34 @@
 import React from 'react';
 
 export default class landingPage extends React.Component {
+    state = {
+        zipcode: '',
+    }
+    handleChange = (event) => {
+        this.setState({
+            zipcode: event.target.value,
+        })
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.history.push(`/${this.state.zipcode}`)
+    }
     render() {
+        console.log('Landing Page Props', this.props)
         return (
             <div className="center container">
                 <div className="row">
-                    <form className="col s12">
+                    <form onSubmit={this.handleSubmit} className="col s12">
                         <div className="input-field col s12">
-                            <label className="active" htmlFor="first_name">KILL ME</label>
-                            <input placeholder="Placeholder" id="first_name" type="text" className="validate" />
+                            <label className="active" htmlFor="zipcode">KILL ME</label>
+                            <input
+                                placeholder="Placeholder"
+                                id="zipcode"
+                                type="text"
+                                className="validate"
+                                value={this.state.zipcode}
+                                onChange={this.handleChange}
+                            />
                         </div>
                         <button className="btn waves-effect waves-light">KILL ME</button>
                     </form>
