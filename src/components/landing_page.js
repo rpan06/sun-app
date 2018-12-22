4808f6af2900c84991ab4f46ceab6892a748deef
krow <rpan06@gmail.com>
2018-12-21 16:53:55 -0800

    stashing changes

--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import zipcodeConversions from './zipcodeConversions';
 export default class landingPage extends React.Component {
     state = {
         zipcode: '',
@@ -11,29 +11,51 @@ export default class landingPage extends React.Component {
     }
     handleSubmit = (event) => {
         event.preventDefault();
-        const {zipcode} = this.state;
+
+        const { zipcode } = this.state;
+        if (!/^(\d{5})$/.test(zipcode) && this.checkSubmit()) {
+            console.log('FAILS CHECK')
+            return;
+        }
+
         //doublecheck that zipcode is okay
         //5 numbers  /d{5}
         //check in list of JS
         //or else ERROR
         this.props.history.push(`/${this.state.zipcode}`)
     }
+    checkSubmit() {
+        const { zipcode } = this.state;
+        //check that zipcode is in system;
+        for (let key in zipcodeConversions) {
+            // console.log(zipcode, key)
+            if (zipcode === key) {
+                return true;
+            }
+        }
+        return false;
+    }
     render() {
-        console.log('Landing Page Props', this.props)
+        // console.log('Landing Page Props', this.props)
         return (
             <div className="center container">
                 <div className="row">
+                    <h1>Sunny D3</h1>
+                </div>
+                <div className="row">
                     <form onSubmit={this.handleSubmit} className="col s12">
-                        <div className="input-field col s12">
-                            <label className="active" htmlFor="zipcode">KILL ME</label>
-                            <input
-                                placeholder="Placeholder"
-                                id="zipcode"
-                                type="text"
-                                className="validate"
-                                value={this.state.zipcode}
-                                onChange={this.handleChange}
-                            />
+                        <div className="row">
+                            <div className="input-field col s12 m6 offset-m3">
+                                {/* <label className="active" htmlFor="zipcode">KILL ME</label> */}
+                                <input
+                                    placeholder="Enter zipcode here"
+                                    id="zipcode"
+                                    type="text"
+                                    className="validate"
+                                    value={this.state.zipcode}
+                                    onChange={this.handleChange}
+                                />
+                            </div>
                         </div>
                         <button className="btn waves-effect waves-light">KILL ME</button>
                     </form>
